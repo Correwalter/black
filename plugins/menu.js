56694f461ecf366b7b53aa6af7bc6810080ce029
Correwalter <111578841+Correwalter@users.noreply.github.com>
2022-08-30 22:45:30 -0400

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -62,8 +62,17 @@ let menu = `
 ┣ ඬ⃟💫 _${usedPrefix}earrape *[nota de voz]*_
 ┣ ඬ⃟💫 _${usedPrefix}reverse *[nota de voz]*_
 ┣ ඬ⃟💫 _${usedPrefix}smooth *[nota de voz]*_
+
+
+
+
+
+
+
+
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 ┃ *< OWNER >*
+
 ┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 ┣ ඬ⃟💫 _${usedPrefix}update_
 ┣ ඬ⃟💫 _${usedPrefix}banchat_
